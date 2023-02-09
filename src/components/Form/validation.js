d00aa53987f82e75c4bdb8e07c1e7dda91269ff9
Paulo Castillo <xpropollo@gmail.com>
2023-02-08 22:24:03 -0400

    rick_and_morty updating files

--- src/components/Form/validation.js
+++ src/components/Form/validation.js
@@ -1,12 +1,9 @@
 export default function validation (inputs){
     const errors = {};
+    
     const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-
     const regexpassword =  /^(?=\w*\d)\S{6,10}$/;
-
     const regexnumber =  /^(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$/i
-
-
 
     if(!regexEmail.test(inputs.username)){
         errors.username = 'Debe ser correo electronico'
